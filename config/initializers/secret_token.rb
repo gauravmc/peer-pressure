e7e4de4d763009cacb558958ba704828ff6f2dfe
Gaurav Chande <gmail@gauravchande.com>
2012-07-19 18:58:02 +0530

    First commit: Installed shopify_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PeerPressure::Application.config.secret_token = 'f95577040517028eea00cc4f232e70c71840c72f237503bc299819a56f9bace8aa48ed34ab912df396d22b40fafbc5564dba892230510f5f1777ab5246cfcc0b'
